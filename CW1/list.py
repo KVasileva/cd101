16c1accc200318444d1aa083831c0d29d4df7cae
KVasileva <gpost6660@gmail.com>
2021-02-24 09:41:00 +0300

    CW1_v1.1

---
+++ CW1/list.py
@@ -0,0 +1,27 @@
+#nums = [13, 4, 99, 8, 50, 5, 66, 5, 7, 22, 8, 88, 20]
+
+for n in range (0, 10, 2):
+ print ("Hello")
+
+   
+print (sum)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+# input_string = input("Enter family members separated by comma ")
+# family_list  = input_string.split(",")
+# print("Printing all family member names")
+# for name in family_list:
+#     print(name)
+
+lst = [ ]
+n = int(input("Enter number of elements : "))
+for i in range(0, n):
+    ele = [input(), int(input())]
+lst.append(ele)
+print(lst)
+#a[len(a)-1] = 11
+
---
+++ CW1/mutability.py
@@ -0,0 +1,8 @@
+x=10 #immutable
+y=x
+name= "pablo"
+nickname = name
+name= "Yo"
+x=20 
+
+print (name)
