d13ac9359728752511fc93bb59280c832c7e93c6
f4kekun <f4kekun@yandex.ru>
2021-02-25 15:08:28 +0300

    CW2_v1.0

---
+++ list2.py
@@ -0,0 +1,37 @@
+data= []
+ages= []
+data = input("Enter name ang age separeted with space").split( )
+for i in data:
+    data.append(i)
+print (f"{data}")  
+
+# x=11
+# if x%2==0 :
+#     print("x is even")
+# else:
+#     print("x is not even")
+
+# lst = [1, 289, -84, 4, 3, 8,-9]
+# for i in lst:
+#     if i%2==0:
+#         print (f"{i} even")
+#     elif i%3==0:
+#         print(f"{i} %3 true")
+#     else:
+#         print(f"{i} not even")
+
+# for i in lst:
+#     if i%2==0 and i>0:
+#         print (f"{i} is even and >0")
+#     else:
+#         print(f"{i} is not")
+
+# evn = []
+# nevn = []
+# for i in lst:
+#     if i%2==0:
+#         evn.append(i)
+#     else:
+#         nevn.append(i)
+
+# print (f"{evn} is even, {nevn} is not")
