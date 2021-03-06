d90dc85cc0b975d19ca4fff3bd64c8271946a9ed
anastasiasibiryakova <48712029+anastasiasibiryakova@users.noreply.github.com>
2021-03-06 17:27:36 +0300

    HW2_v1.2
    Не рабочая

--- ifcomp.py
+++ ifcomp.py
@@ -1,33 +1,35 @@
-def wultc (com):
-    while True: 
-        com_data1 = input("first name, last name, birth age, death age separated by space:\n").split(" ")
-        status  = input ("Would you like to continue? If yes press Y, if no press N  ")
+
+
+def inp ():
+    names=[]
+    ages=[]
+    com_data1 = []
+    com_data1 = input("first name, last name, birth age, death age separated by space:\n").split(" ")
+    names.append(com_data1)
+    a = int(com_data1[3])-int(com_data1[2])
+    ages.append(a)
+    return (f"{names}, {ages}")
+
+def wultc ():     
+    while True:          
+        com = inp()             
+        status  = input ("Would you like to continue? If yes press Y, if no press N  ")        
         if status == "N":
             break
         else:
             continue
-    return com
-# com_data = input("first name, last name, birth age, death age separated by space:\n").split(" ") 
-
-# print (wultc(com_data))
 
 
-names = []
-com_data = []
-ages=[]
-a=0
-  
+print (inp())
+
+
+
+
 #com_data = input("first name, last name, birth age, death age separated by space:\n").split(" ")
-b = wultc(com_data)
-for i in range (len(b)-1):
-    
-    names.append(b[1])
-    a = int(b[3])-int(b[2])
-    ages.append(a)
-#age_sum = age_sum+ int(data[1])
 
-for idx, val in enumerate(names):
-     print (f"User {idx}: {val}, {ages[idx]}")
+
+# for idx, val in enumerate(names):
+#      print (f"User {idx}: {val}, {ages[idx]}")
 
 # for i in range (n):
 #     a = int(com_list[i][3])-int(com_list[i][2])
